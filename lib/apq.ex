6c880c791b84b1ca9dbb20b98975413205ae3ed8
Maarten van Vliet <maartenvanvliet@gmail.com>
2021-11-13 12:52:09 +0100

    Add Readme to main module

--- lib/apq.ex
+++ lib/apq.ex
@@ -1,14 +1,9 @@
 defmodule Apq do
-  @moduledoc """
-  Provide support for [automatic persisted queries](https://www.apollographql.com/docs/guides/performance.html#automatic-persisted-queries)
-
-  When a request contains the apq extension and the query-document is not provided we do
-  a lookup in a cache using a hash to fetch the document. If it is available the normal graphql
-  resolving starts, if not we return an error.
-
-  The graphql-client should retry when it sees the error, but now with the query so
-  we can store the query in the cache using the hash as a cache key.
-  """
+  @external_resource "./README.md"
+  @moduledoc @external_resource
+             |> File.read!()
+             |> String.split("<!-- MDOC !-->")
+             |> Enum.fetch!(1)
 
   defstruct [:error, :action, :document, :digest]
 
